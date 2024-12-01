85f041afadaf20ec156e810b1b41fde196b407e9
Naz <bluspringindohq@gmail.com>
2024-12-01 17:21:09 +0800

    Add inventory and dish managers

---
+++ src/util/dishes.py
@@ -0,0 +1,45 @@
+import json
+
+class Dish:
+    dishName: str
+    items: list[str] # List of InventoryItem names
+    price: float
+
+    def __init__(self, dishName: str, items: list[str], price: float):
+        self.dishName = dishName
+        self.items = items
+        self.price = price
+
+class DishEncoder(json.JSONEncoder):
+    def default(self, o: Dish):
+        return o.__dict__
+
+def decode_dish(obj: dict) -> Dish:
+    return Dish(obj["dishName"], obj["items"], obj["price"])
+
+class DishManager:
+    dishes: list[Dish]
+
+    def __init__(self):
+        self.dishes = []
+        self.load()
+
+    def save(self):
+        with open("dishes.json", "w") as file:
+            json.dump(self.dishes, file, indent = 4, cls = DishEncoder)
+
+    def load(self):
+        try:
+            with open("dishes.json", "r") as file:
+                data = json.load(file, object_hook = decode_dish)
+                self.dishes = data
+        except FileNotFoundError:
+            # Ignore non-existing files
+            pass
+
+    def get_dish(self, name: str) -> Dish | None:
+        for item in self.dishes:
+            if item.dishName == name:
+                return item
+
+        return None
---
+++ src/util/inventory.py
@@ -0,0 +1,45 @@
+import json
+
+class InventoryItem:
+    itemName: str
+    price: float
+    amount: int
+
+    def __init__(self, itemName: str, price: float, amount: int):
+        self.itemName = itemName
+        self.price = price
+        self.amount = amount
+
+class ItemEncoder(json.JSONEncoder):
+    def default(self, o: InventoryItem):
+        return o.__dict__
+
+def decode_item(obj: dict) -> InventoryItem:
+    return InventoryItem(obj["itemName"], obj["price"], obj["amount"])
+
+class InventoryManager:
+    items: list[InventoryItem]
+
+    def __init__(self):
+        self.items = []
+        self.load()
+
+    def save(self):
+        with open("inventory.json", "w") as file:
+            json.dump(self.items, file, indent = 4, cls = ItemEncoder)
+
+    def load(self):
+        try:
+            with open("inventory.json", "r") as file:
+                data = json.load(file, object_hook = decode_item)
+                self.items = data
+        except FileNotFoundError:
+            # Ignore non-existing files
+            pass
+
+    def get_item(self, name: str) -> InventoryItem | None:
+        for item in self.items:
+            if item.itemName == name:
+                return item
+
+        return None
