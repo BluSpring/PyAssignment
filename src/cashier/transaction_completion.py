dc96b384c6d8709f79d7e7a68ab01617b3107f69
Naz <bluspringindohq@gmail.com>
2024-12-04 01:27:25 +0800

    Fix up transaction completion

--- src/cashier/transaction_completion.py
+++ src/cashier/transaction_completion.py
@@ -1,13 +1,30 @@
-from util.accounts import Account
+from manager.order_management import Order
+from util.accounts import Account, AccountManager
+from util.dishes import DishManager
+from util.orders import OrderManager
+
 
 # Transaction Completion - Cashier
-class Transaction:
-    def _init_(self):
-        self.transactions = []
+def complete_transaction(order: Order):
+    dishManager = DishManager()
+    accountManager = AccountManager()
+    account = accountManager.get_account("customer", order.customer_name)
 
-    def complete_transaction(self, transaction_details):
-        self.transactions.append(transaction_details)
-        print(f"Transaction completed: {transaction_details}")
+    print(f"Receipt for Order #{order.order_id}")
+    print(f"  Customer Name: {account.name}")
+
+    for item in order.items:
+        dish = dishManager.get_dish_lenient(item)
+        print(f"  - {dish.dishName}: ${dish.get_price()}")
 
 def init(account: Account):
+    orderManager = OrderManager()
+    orderId = int(input("Insert an order ID: "))
+    order = orderManager.get_order(orderId)
+
+    if order is None:
+        raise Exception(f"Order does not exist!")
+
+    complete_transaction(order)
+
     pass
