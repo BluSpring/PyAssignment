3d6322f0f43438cfaf420b46e6f6c8dce0587280
Naz <bluspringindohq@gmail.com>
2024-12-04 12:47:26 +0800

    Rework production record keeping

--- src/chef/production_record_keeping.py
+++ src/chef/production_record_keeping.py
@@ -1,26 +1,92 @@
-from util.accounts import Account
+import json
 
-def init(account: Account):
-    pass
+from util.accounts import Account
+from util.dishes import DishManager
+from util.id_manager import IdManager
+from util.menu import OptionMenu
+from util.pagination import Manager, create_pagination
+from util.utils import millis_to_formatted_date_time, get_current_time_millis
+
 
 class ProductionRecord:
-    def __init__(self, recipe_name, quantity, batch_number, date):
-        self.recipe_name = recipe_name
+    dishName: str
+    quantity: int
+    batchNumber: int
+    productionTimestamp: int
+
+    def __init__(self, dishName, quantity, batchNumber, productionTimestamp):
+        self.dishName = dishName
         self.quantity = quantity
-        self.batch_number = batch_number
-        self.date = date
+        self.batchNumber = batchNumber
+        self.productionTimestamp = productionTimestamp
 
-    def __str__(self):
-        return f"Recipe: {self.recipe_name}, Quantity: {self.quantity}, Batch: {self.batch_number}, Date: {self.date}"
+    def get_expiry_timestamp(self):
+        return self.productionTimestamp + 2.592e+8 # 3 days
 
-production_records = []
+class ItemEncoder(json.JSONEncoder):
+    def default(self, o: ProductionRecord):
+        return o.__dict__
 
-def log_production(recipe_name, quantity, batch_number, date):
-    record = ProductionRecord(recipe_name, quantity, batch_number, date)
-    production_records.append(record)
-    print(f"Production logged: {record}")
+def decode_item(obj: dict) -> ProductionRecord:
+    return ProductionRecord(obj["dishName"], obj["quantity"], obj["batchNumber"], obj["productionTimestamp"])
 
-def view_production_records():
+class ProductionRecordManager(Manager[ProductionRecord]):
+    records: list[ProductionRecord]
 
-    for record in production_records:
-        print(record)
+    def __init__(self):
+        self.records = []
+        self.load()
+
+    def save(self):
+        with open("production.json", "w") as file:
+            json.dump(self.records, file, indent = 4, cls = ItemEncoder)
+
+    def load(self):
+        try:
+            with open("production.json", "r") as file:
+                data = json.load(file, object_hook = decode_item)
+                self.records = data
+        except FileNotFoundError:
+            # Ignore non-existing files
+            pass
+
+    def get_batch_by_id(self, id: int) -> ProductionRecord | None:
+        for record in self.records:
+            if record.batchNumber == id:
+                return record
+
+        return None
+
+def init(account: Account):
+    recordManager = ProductionRecordManager()
+    menu = OptionMenu("Production Records")
+
+    menu.add_option("View Records", lambda: view_production_records(recordManager))
+    menu.add_option("Log Production Record", lambda: handle_log_record(recordManager))
+
+    menu.process()
+
+def handle_log_record(recordManager: ProductionRecordManager):
+    dishManager = DishManager()
+    idManager = IdManager()
+
+    dishName = input("Insert a dish name: ")
+    dish = dishManager.get_dish_lenient(dishName)
+
+    if dish is None:
+        raise Exception("No dish exists by that name!")
+
+    quantity = int(input("Insert a quantity: "))
+
+    if quantity <= 0:
+        raise Exception("Production quantity must be higher than 0!")
+
+    id = idManager.get_and_increment_id("production")
+    record = ProductionRecord(dishName, quantity, id, get_current_time_millis())
+
+    recordManager.records.append(record)
+    print(f"Production logged for Batch #{record.batchNumber}.")
+
+
+def view_production_records(recordManager: ProductionRecordManager):
+    create_pagination(recordManager, "Production Records", (lambda record: f"Batch #{record.batchNumber}, creating {record.quantity} x {record.dishName}, expiring on {millis_to_formatted_date_time(record.get_expiry_timestamp())}"), None, 0)
