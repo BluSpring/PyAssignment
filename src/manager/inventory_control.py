9a7cf9c9c74d539d316948d117180cb520f6b142
Naz <bluspringindohq@gmail.com>
2024-12-04 12:59:53 +0800

    Use custom pagination system for inventory control

--- src/manager/inventory_control.py
+++ src/manager/inventory_control.py
@@ -1,68 +1,17 @@
-import math
-
 from util.accounts import Account
 from util.dishes import DishManager
 from util.inventory import InventoryManager
 from util.menu import OptionMenu
+from util.pagination import create_pagination
 
 
 # Dishes inventory
 def display_dishes(dishManager: DishManager, page: int):
-    maxPages = math.floor(len(dishManager.dishes) / 10)
-
-    # The index to use for printing the orders array
-    itemsStart = page * 10
-
-    # Only list up to a max of 10 orders.
-    totalDishesToDisplay = min(10, len(dishManager.dishes) - itemsStart)
-
-    productsMenu = OptionMenu(f"Dish Inventory Control")
-    productsMenu.automaticallyExit = True
-    productsMenu.description = ""
-
-    for i in range(itemsStart, itemsStart + totalDishesToDisplay):
-        dish = dishManager.dishes[i]
-        productsMenu.description += f"\n  {i + 1}. {dish.dishName} - ${dish.get_price()}"
-
-    productsMenu.description += f"\n\nShowing {totalDishesToDisplay} items out of {len(dishManager.dishes)}."
-    productsMenu.description += f"\nPage {page + 1} / {maxPages}"
-
-    if page > 0:
-        productsMenu.add_option("Previous Page", lambda: display_dishes(dishManager, page - 1))
-
-    if page < maxPages - 1:
-        productsMenu.add_option("Next Page", lambda: display_dishes(dishManager, page + 1))
-
-    productsMenu.process()
+    create_pagination(dishManager, "Dish Inventory Control", dishManager.dishes, (lambda dish: f"{dish.dishName} - ${dish.price}"), None, page)
 
 # Ingredients inventory
 def display_inventory(inventoryManager: InventoryManager, page: int):
-    maxPages = math.floor(len(inventoryManager.items) / 10)
-
-    # The index to use for printing the orders array
-    itemsStart = page * 10
-
-    # Only list up to a max of 10 orders.
-    totalItemsToDisplay = min(10, len(inventoryManager.items) - itemsStart)
-
-    productsMenu = OptionMenu(f"Ingredient Inventory Control")
-    productsMenu.automaticallyExit = True
-    productsMenu.description = ""
-
-    for i in range(itemsStart, itemsStart + totalItemsToDisplay):
-        item = inventoryManager.items[i]
-        productsMenu.description += f"\n  {i + 1}. {item.itemName} - ${item.amount}x"
-
-    productsMenu.description += f"\n\nShowing {totalItemsToDisplay} items out of {len(inventoryManager.items)}."
-    productsMenu.description += f"\nPage {page + 1} / {maxPages}"
-
-    if page > 0:
-        productsMenu.add_option("Previous Page", lambda: display_inventory(inventoryManager, page - 1))
-
-    if page < maxPages - 1:
-        productsMenu.add_option("Next Page", lambda: display_inventory(inventoryManager, page + 1))
-
-    productsMenu.process()
+    create_pagination(inventoryManager, "Ingredient Inventory Control", inventoryManager.items, (lambda item: f"{item.itemName} - {item.amount} units"), None, page)
 
 def init(account: Account):
     inventoryManager = InventoryManager()
