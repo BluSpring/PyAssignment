d80ffd8a5043de6725556bd3a44021e9b9b7d397
Naz <bluspringindohq@gmail.com>
2024-12-04 16:15:15 +0800

    Make product browsing paginated

--- src/customer/product_browsing.py
+++ src/customer/product_browsing.py
@@ -1,5 +1,6 @@
 from util.accounts import Account
 from util.dishes import DishManager
+from util.pagination import create_pagination
 
 
 # Product Browsing - Customer
@@ -8,16 +9,18 @@ class ProductBrowser:
         self.products = products
 
     def browse_products(self):
-        print("Available Products:")
-        for product in self.products:
-            print(f"- {product}")
+        create_pagination(None, "Available Products", self.products, (lambda text: text), None, 0)
 
 def init(account: Account):
     dishManager = DishManager()
     products = []
 
     for dish in dishManager.dishes:
-        text = f"{dish.dishName} - ${dish.price}"
+        text = f"{dish.dishName} - ${dish.get_price()}"
+
+        if dish.currentDiscount > 0:
+            text += f" ({dish.currentDiscount * 100}% off, was ${dish.price})"
+
         if not dishManager.is_dish_available(dish):
             text = f"(unavailable) {text}"
 
