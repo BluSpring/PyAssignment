61c68ab50c3a0dc2079b06a8bd82979c52e2d923
Naz <bluspringindohq@gmail.com>
2024-12-05 09:42:40 +0800

    Rework the main functions so the root directory can always be the working dir.

---
+++ main.py
@@ -0,0 +1,5 @@
+from src.main import main
+
+# Call the main function in src. If the src/main.py file was run directly, it may cause issues unless the working directory was manually set to the root directory,
+# so it's easier to have the root directory as the working directory immediately.
+main()
--- src/main.py
+++ src/main.py
@@ -4,18 +4,25 @@ import customer
 import manager
 from util.menu import OptionMenu
 
-print("--[  Amar's Restaurant  ]--")
+def main():
+    print("--[  Amar's Restaurant  ]--")
 
-# Create the starting menu screen.
-selection = OptionMenu(name = "Select Login Type")
-selection.description = "Welcome to Amar's Restaurant's ordering and management system!"
-selection.description += "\nPlease select your corresponding login type to continue."
+    # Create the starting menu screen.
+    selection = OptionMenu(name = "Select Login Type")
+    selection.description = "Welcome to Amar's Restaurant's ordering and management system!"
+    selection.description += "\nPlease select your corresponding login type to continue."
 
-# Add all login types as options, with their initializing functions to be executed when selected.
-selection.add_option("Manager", manager.init)
-selection.add_option("Chef", chef.init)
-selection.add_option("Cashier", cashier.init)
-selection.add_option("Customer", customer.init)
+    # Add all login types as options, with their initializing functions to be executed when selected.
+    selection.add_option("Manager", manager.init)
+    selection.add_option("Chef", chef.init)
+    selection.add_option("Cashier", cashier.init)
+    selection.add_option("Customer", customer.init)
 
-# Display the menu onto the screen
-selection.process()
+    # Display the menu onto the screen
+    selection.process()
+
+# If the file was run directly here, just allow it.
+# The main() function was added so it could be called by the root main.py, as otherwise the program may not work correctly
+# if it was run directly here, unless the working directory was manually set to the root directory.
+if __name__ == "__main__":
+    main()
